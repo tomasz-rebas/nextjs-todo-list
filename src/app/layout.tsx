d00d807f6553dfa189ffb651536dc416285c88f1
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-04-27 17:31:37 +0200

    Use React Query to fetch data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from "./providers";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,34 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+interface ToDo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId?: number;
+}
+
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
+
+const fetchData = async (): Promise<ToDo[]> => {
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  const data: ToDo[] = await response.json();
+
+  return data.map(({ userId, ...rest }) => rest);
+};
+
 export default function Home() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["data"],
+    queryFn: fetchData,
+  });
+
   return (
     <div>
       <h1 className="text-4xl text-center my-8">ToDo List</h1>
