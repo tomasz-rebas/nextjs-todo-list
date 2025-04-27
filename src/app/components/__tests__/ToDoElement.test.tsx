e1e2c2067b313a766fba2de65d07c38209e1eefc
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-04-27 21:55:59 +0200

    Configure Jest and provide a test for ToDoElement

---
+++ src/app/components/__tests__/ToDoElement.test.tsx
@@ -0,0 +1,55 @@
+import { getByText, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { ToDoElement } from "../ToDoElement";
+import "@testing-library/jest-dom";
+
+describe("ToDoElement", () => {
+  it("renders unfinished ToDo", () => {
+    const toDo = { id: 1, title: "Test ToDo", completed: false };
+    const setToDos = jest.fn();
+
+    render(<ToDoElement toDo={toDo} setToDos={setToDos} />);
+
+    const checkbox = screen.getByRole("checkbox");
+    const label = screen.getByText("Test ToDo");
+
+    expect(checkbox).not.toBeChecked();
+    expect(label).toBeInTheDocument();
+  });
+
+  it("renders completed ToDo", () => {
+    const toDo = { id: 2, title: "Second ToDo", completed: true };
+    const setToDos = jest.fn();
+
+    render(<ToDoElement toDo={toDo} setToDos={setToDos} />);
+
+    const checkbox = screen.getByRole("checkbox");
+    const label = screen.getByText("Second ToDo");
+
+    expect(checkbox).toBeChecked();
+    expect(label).toBeInTheDocument();
+  });
+
+  it("toggles checkbox", async () => {
+    const toDo = { id: 1, title: "Test todo", completed: false };
+    const setToDos = jest.fn();
+
+    render(<ToDoElement toDo={toDo} setToDos={setToDos} />);
+
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).not.toBeChecked();
+
+    await userEvent.click(checkbox);
+
+    expect(setToDos).toHaveBeenCalledTimes(1);
+    expect(setToDos).toHaveBeenCalledWith(expect.any(Function));
+
+    const callback = setToDos.mock.calls[0][0];
+
+    const updatedState = callback([toDo]);
+
+    expect(updatedState).toEqual([{ ...toDo, completed: true }]);
+
+    expect(checkbox).toBeChecked();
+  });
+});
