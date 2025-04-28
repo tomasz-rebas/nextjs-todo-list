ecd1d6de66eabdaad0ac665da13a0e3de230fe31
tomasz-rebas <42824472+tomasz-rebas@users.noreply.github.com>
2025-04-28 02:12:39 +0200

    Get rid of let variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ import { ToDo } from "./types";
 const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 export default async function Home() {
-  let initialData: ToDo[] = [];
-  let error: string | null = null;
-
   try {
     const response = await fetch(API_URL);
 
@@ -15,11 +12,11 @@ export default async function Home() {
     }
 
     const data: ToDo[] = await response.json();
-    initialData = data.map(({ userId, ...rest }) => rest);
+    const initialData = data.map(({ userId, ...rest }) => rest);
+
+    return <ToDoList initialData={initialData} error={null} />;
   } catch (err) {
     console.error(err);
-    error = "Failed to fetch the data.";
+    return <ToDoList initialData={[]} error="Failed to fetch the data." />;
   }
-
-  return <ToDoList initialData={initialData} error={error} />;
 }
